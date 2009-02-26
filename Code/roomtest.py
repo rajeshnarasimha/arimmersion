23fc9374bba2b94813a9b45b3d99020cc812277f
jonathan.d.ventura <jonathan.d.ventura@131b0e12-0158-11de-b507-8b4ecd5210be>
2009-02-26 00:36:58 +0000

    Added eucalyptus environment map from debevec.org

--- Code/roomtest.py
+++ Code/roomtest.py
@@ -2,7 +2,11 @@ import viz
 
 viz.go()
 #viz.add( 'tut_ground.wrl' )
-room = viz.add('../models/room/crappy_smallroom1.wrl')
+room = viz.add('../models/room/crappy_smallroom1.wrl',viz.WORLD,1)
 room.setPosition( [0,-2,0] )
 room.setEuler( [0, 90, 0] );
 room.setScale( .05, .05, .05 )
+
+env = viz.add(viz.ENVIRONMENT_MAP, 'eucalyptus\eucalyptus.jpg')
+sky = viz.add('skydome.dlc')
+sky.texture(env)
