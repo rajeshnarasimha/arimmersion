5e65d149e41ca726e5cd83ace489ced674ae4f27
jonathan.d.ventura <jonathan.d.ventura@131b0e12-0158-11de-b507-8b4ecd5210be>
2009-03-10 03:14:52 +0000

    adding analysis directory

---
+++ Analysis/script.R
@@ -0,0 +1,21 @@
+datafilename="alldata.csv"
+data.all=read.csv(datafilename,header=T)	# read the data into a table
+
+# one-way anova on fov
+aov.fov=aov(percent_correct~fov,data.all)
+print(summary(aov.fov))
+#print(model.tables(aov.all,"means"),digits=3)
+boxplot(percent_correct~fov,data=data.all)
+
+
+# one-way anova on latency
+aov.latency=aov(percent_correct~latency,data.all)
+print(summary(aov.latency))
+boxplot(percent_correct~latency,data=data.all)
+
+
+
+# two-way anova on both
+aov.two = aov(percent_correct~fov*latency,data.all)
+print(summary(aov.two))
+boxplot(percent_correct~fov*latency,data=data.all)
