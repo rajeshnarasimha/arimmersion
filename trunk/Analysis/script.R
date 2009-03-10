33fdcf5867d6353737b47b13f7da505f7b51ec1f
jonathan.d.ventura <jonathan.d.ventura@131b0e12-0158-11de-b507-8b4ecd5210be>
2009-03-10 05:54:37 +0000

    added condition number to data

--- trunk/Analysis/script.R
+++ trunk/Analysis/script.R
@@ -1,16 +1,24 @@
 datafilename="alldata.tab"
 data.all=read.table(datafilename,header=T)	# read the data into a table
 
-# one-way repeated measures anova on fov
-aov.fov=aov(recall~fov+Error(id/fov),data.all)
+aov.fov=aov(recall~condition+Error(id/condition),data.all)
 print(summary(aov.fov))
-boxplot(recall~fov,data=data.all)
+boxplot(recall~condition,data=data.all)
+
+aov.fov=aov(precision~condition+Error(id/condition),data.all)
+print(summary(aov.fov))
+boxplot(precision~condition,data=data.all)
+
+# one-way repeated measures anova on fov
+#aov.fov=aov(recall~fov+Error(id/fov),data.all)
+#print(summary(aov.fov))
+#boxplot(recall~fov,data=data.all)
 
 
 # one-way repeated measures anova on latency
-aov.latency=aov(recall~latency+Error(id/latency),data.all)
-print(summary(aov.latency))
-boxplot(recall~latency,data=data.all)
+#aov.latency=aov(recall~latency+Error(id/latency),data.all)
+#print(summary(aov.latency))
+#boxplot(recall~latency,data=data.all)
 
 
 
@@ -30,25 +38,24 @@ detach(data.all)
 
 
 
-
 # one-way repeated measures anova on precision~fov
-aov.fov=aov(precision~fov+Error(id/fov),data.all)
-print(summary(aov.fov))
+#aov.fov=aov(precision~fov+Error(id/fov),data.all)
+#print(summary(aov.fov))
 #boxplot(recall~fov,data=data.all)
 
 
 # one-way repeated measures anova on precision~latency
-aov.latency=aov(precision~latency+Error(id/latency),data.all)
-print(summary(aov.latency))
+#aov.latency=aov(precision~latency+Error(id/latency),data.all)
+#print(summary(aov.latency))
 #boxplot(recall~latency,data=data.all)
 
 
 
 # two-way anova repeated measures on precision~(fov*latency)
-aov.two = aov(precision~(fov*latency)+Error(id/(fov*latency)),data.all)
-print(summary(aov.two))
+#aov.two = aov(precision~(fov*latency)+Error(id/(fov*latency)),data.all)
+#print(summary(aov.two))
 #boxplot(recall~fov*latency,data=data.all)
-attach(data.all)
-interaction.plot(fov,latency,precision)    #another way to graph the interaction
-detach(data.all)
+#attach(data.all)
+#interaction.plot(fov,latency,precision)    #another way to graph the interaction
+#detach(data.all)
 
