a2d4961c4f9fed8af6bd5bd21567d5b59323e7e2
marcusjang <marcusjang@131b0e12-0158-11de-b507-8b4ecd5210be>
2009-05-01 02:34:34 +0000

    timeline gen is refactored a bit more to be cleaner with slightly fewer bugs

--- Code/TimelineGen.py
+++ Code/TimelineGen.py
@@ -7,10 +7,6 @@ import time
 #function creates timeline based on the global settings, with randomized times for
 #live events.  All live events will have at least the minimum specified length of minLiveLength
 
-
-
-
-
 class Event:
 	def __init__(self, isDead = False, length = 0):
 		self.isDead = isDead
@@ -33,37 +29,39 @@ class Event:
 
 class Timeline:
 	#global settings
-	trialTime = 45	#total time of a trial in seconds
-	numDead = 4		#total number of dropouts
-	deadLength = 2	#time of each dropout
-	minLiveLength = 4	#mininum time of each live (or non-dropout) chunk of time
-	totalEvents = (numDead * 2) + 1	#total number of events in trial (both dropouts and non-dropouts)
-	minTotalTime = (numDead * deadLength) + (minLiveLength * numDead)
-	if trialTime < minTotalTime:
-			print "Bad settings, not enough trial time, need at least: " + str(minTotalTime) + " seconds"
+	trialTime = 45									#total time of a trial in seconds
+	numDead = 4										#total number of dropouts
+	deadLength = 2									#time of each dropout
+	minLiveLength = 4								#mininum time of each live (or non-dropout) chunk of time
+	totalEvents = (numDead * 2) + 1					#total number of events in trial (both dropouts and non-dropouts)
+	minTotalTime = 0;
 	
 	def __init__(self):
 		self.onlyLiveTime = []
 		self.timeline = []
 		self.timeLeft = Timeline.trialTime
+		Timeline.minTotalTime = (Timeline.numDead * Timeline.deadLength) + (Timeline.minLiveLength * Timeline.numDead)
+		if Timeline.trialTime < Timeline.minTotalTime:
+			print "Bad settings, not enough trial time, need at least: " + str(Timeline.minTotalTime) + " seconds"
+			sys.exit(0)
 		
 		for i in range(Timeline.totalEvents):
 			if(i % 2 == 0):
-				if(i == Timeline.totalEvents-1):	#create an empty live event at the end of the list
+				if(i == Timeline.totalEvents-1):						#create an empty live event at the end of the list
 					liveEvent = Event(False, 0)
 					self.timeline.append(liveEvent)
 					self.onlyLiveTime.append(liveEvent)
-				else:
+				else:													#create live event
 					self.timeLeft -= Timeline.minLiveLength
-					liveEvent = Event(False, Timeline.minLiveLength) #create live event
+					liveEvent = Event(False, Timeline.minLiveLength) 
 					self.timeline.append(liveEvent)
 					self.onlyLiveTime.append(liveEvent)
-			else:
+			else:														#create dropout event
 				self.timeLeft -= Timeline.deadLength
-				self.timeline.append(Event(True, Timeline.deadLength)) #create dropout event
-		self.timeline[Timeline.totalEvents-1].setLength(0)
+				self.timeline.append(Event(True, Timeline.deadLength)) 
 
 		#randomize length of live events
+		random.seed(time.time())
 		for x in range(self.timeLeft):
 			index = random.randrange(0, len(self.onlyLiveTime))
 			self.onlyLiveTime[index].addTime()
@@ -77,23 +75,24 @@ class Timeline:
 	def getTimeLeft(self):
 		return self.timeLeft		
 
-def printLiveEvents(timeline):
-	liveEvents = timeline.getOnlyLiveEvents()
-	for i in range(len(liveEvents)):
-		print liveEvents[i]
+	def printLiveEvents(self):
+		for event in self.onlyLiveTime:
+			print event
 
-def printAllEvents(timeline):
-	allEvents = timeline.getAllEvents()
-	for i in range(len(allEvents)):
-		print allEvents[i]
+	def printAllEvents(self):
+		for event in self.timeline:
+			print event
 
+#create timeline
 timeline = Timeline()
 
-filename = "pickleTimeline" + str(int(time.time()) - 1240892802)
+#pickle the timeline to disk
+filename = "pickleTimeline" + str(int(time.time()) - 1240999999)
 fw = open(filename, "w")
 pickle.dump(timeline, fw)
 fw.close()
 
+#test opening the pickled file and printing info to screen
 fr = open(filename, "r")
 openedTimeline = pickle.load(fr)
-printAllEvents(openedTimeline)
+openedTimeline.printAllEvents()
