5fc1d2f08920ee2a09e77564ee1723b2adfcc3ac
jonathan.d.ventura <jonathan.d.ventura@131b0e12-0158-11de-b507-8b4ecd5210be>
2009-06-15 17:47:30 +0000

    adding more analysis

---
+++ trunk/NewResults/timetracking.py
@@ -0,0 +1,34 @@
+import sys
+
+done = False
+thresh = 70
+if len(sys.argv) == 2:
+    thresh = int(sys.argv[1])
+
+while not done:
+    for i in xrange(27):
+	line = sys.stdin.readline()
+	if line == "":
+	    done = True
+	    break
+	list = line.rstrip().split(',')
+	id = list[0]
+	fov = list[1]
+	deadlen = list[2]
+	samples = list[3:]
+
+	samples_in = []
+	for j in samples:
+	    try:
+		samples_in.append( float(j) )
+	    except:
+		continue
+
+	#print "%d samples"%len(samples_in)
+
+	sys.stdout.write("%s,%s,%s,"%(id,fov,deadlen))
+	totaltime = 0
+	for j in xrange(60*60):
+	    if samples_in[j] <= thresh:
+		totaltime += 1
+	sys.stdout.write(" %d\n"%totaltime)
