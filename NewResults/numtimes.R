73685da6591c07873cd0b5e23d7a696cbc1892cb
jonathan.d.ventura <jonathan.d.ventura@131b0e12-0158-11de-b507-8b4ecd5210be>
2009-08-25 03:55:42 +0000

    adding numtimes file

---
+++ NewResults/numtimes.R
@@ -0,0 +1,25 @@
+#datafilename="numtimes_extra8_4.csv"
+datafilename="numtimes8_4.csv"
+data.all=read.csv(datafilename,header=FALSE)	# read the data into a table
+
+attach(data.all)
+data.agg = aggregate(V4,list(V1,V2,V3), function(x)mean(x))
+colnames(data.agg) <- c("id","fov","deadlen",'numtimes')
+detach(data.all)
+
+#aov.deadlen = aov(numtimes~deadlen+Error(id/deadlen),data.agg)
+#aov.fov = aov(numtimes~deadlen+Error(id/fov),data.agg)
+aov.two = aov(numtimes~deadlen*fov+Error(id/(deadlen*fov)),data.agg)
+
+#plot(numtimes~deadlen,data.agg,xlab='Dropout Length',ylab='Number of Times Lost')
+#plot(numtimes~fov,data.agg,xlab='Field of View',ylab='Number of Times Lost')
+
+par(mar=c(5,5,5,2),cex.axis=2,cex.lab=2)
+
+
+#attach(data.agg)
+#interaction.plot(fov,deadlen,numtimes,xlab='Field of View',ylab='Number of Times Lost',trace.label='Dropout Len')
+#detach(data.agg)
+
+plot(numtimes~deadlen,data.agg,cex.axis=2,cex.lab=2,xlab='Dropout Length',ylab='Number of Times Lost')
+#plot(numtimes~fov,data.agg,cex.axis=2,cex.lab=2,xlab='Field of View',ylab='Number of Times Lost')
